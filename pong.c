8192ff820090db9df11ab2d599fc7b998d9a6854
prophet satin <prophetsatin@gmail.com>
2023-08-26 23:36:42 +0300

    pong interactive vesion

--- pong.c
+++ pong.c
@@ -1,5 +1,6 @@
-#include <stdio.h>
 #include <ncurses.h>
+#include <stdio.h>
+#include <unistd.h>
 
 #define STR 29
 #define ROW 99
@@ -18,22 +19,48 @@ int main() {
     int ball_i = STR / 2, ball_j = ROW / 2;             // Ball coordinates
     int dir_y = 1, dir_x = 1;                           // Ball directions
     int left_score = 0, right_score = 0;
+    char winner = 'n';
     char input = 'f';  // Start input value
 
+    initscr();  // Init ncurses screen
+
     while (input != 'q') {
-        printf("LEFT PLAYER SCORE = %d, RIGHT PLAYER SCORE = %d", left_score, right_score);
+        nodelay(stdscr, true);
+
         frame(field);
         input = racket(field, &left_racket, &right_racket);
         ball(field, left_racket, right_racket, &ball_i, &ball_j, &dir_x, &dir_y);
 
-        // Ball is at the rackets level && not on the rackets
-        if (ball_j == 1 && ball_i != left_racket && ball_i != left_racket + 1 && ball_i != left_racket - 1)
+        // Ball is at the racket level && not on the racket
+        if (ball_j == 2 && ball_i != left_racket && ball_i != left_racket + 1 && ball_i != left_racket - 1)
             goal(&right_score, &left_racket, &right_racket, &ball_i, &ball_j, &dir_x, &dir_y);
-        else if (ball_j == ROW - 2 && ball_i != right_racket && ball_i != right_racket + 1 &&
+        else if (ball_j == ROW - 3 && ball_i != right_racket && ball_i != right_racket + 1 &&
                  ball_i != right_racket - 1)
             goal(&left_score, &left_racket, &right_racket, &ball_i, &ball_j, &dir_x, &dir_y);
 
+        clear();
+        if (left_score == 5) {
+            winner = 'l';
+            break;
+        } else if (right_score == 5) {
+            winner = 'r';
+            break;
+        }
+        usleep(50 * 1000);
+        mvprintw(0, 0, "LEFT PLAYER SCORE = %d\n", left_score);
+        mvprintw(0, 77, "RIGHT PLAYER SCORE = %d\n", right_score);
+
         draw_field(field);
+        refresh();
+    }
+
+    endwin();  // End ncurses
+
+    // Winner announcement
+    if (winner == 'l') {
+        printf("LEFT PLAYER WON!");
+    } else if (winner == 'r') {
+        printf("RIGHT PLAYER WON!");
     }
 
     return 0;
@@ -56,8 +83,7 @@ void frame(char field[STR][ROW]) {
 }
 
 int racket(char field[STR][ROW], int *left_racket, int *right_racket) {
-    char input;
-    scanf("%c", &input);
+    char input = getch();
 
     // Update rackets coordinates
     switch (input) {
@@ -66,7 +92,7 @@ int racket(char field[STR][ROW], int *left_racket, int *right_racket) {
             break;
         }
         case 'x': {
-            if ((*left_racket) < 27) (*left_racket)++;
+            if ((*left_racket) < 26) (*left_racket)++;
             break;
         }
         case 'j': {
@@ -74,7 +100,7 @@ int racket(char field[STR][ROW], int *left_racket, int *right_racket) {
             break;
         }
         case 'n': {
-            if ((*right_racket) < 27) (*right_racket)++;
+            if ((*right_racket) < 26) (*right_racket)++;
             break;
         }
     }
@@ -91,8 +117,7 @@ int racket(char field[STR][ROW], int *left_racket, int *right_racket) {
     return input;
 }
 
-void ball(char field[STR][ROW], int left_racket, int right_racket, int *ball_i, int *ball_j, int *dir_x,
-          int *dir_y) {
+void ball(char field[STR][ROW], int left_racket, int right_racket, int *ball_i, int *ball_j, int *dir_x, int *dir_y) {
     int i = *ball_i, j = *ball_j;
 
     field[i][j] = 'o';
@@ -140,8 +165,8 @@ void goal(int *score, int *left_racket, int *right_racket, int *ball_i, int *bal
 void draw_field(char field[STR][ROW]) {
     for (int i = 0; i < STR; i++) {
         for (int j = 0; j < ROW; j++) {
-            printf("%c", field[i][j]);
+            printw("%c", field[i][j]);
         }
-        printf("\n");
+        printw("\n");
     }
 }
